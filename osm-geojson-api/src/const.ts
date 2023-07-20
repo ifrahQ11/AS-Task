f1b357966c5297089081c80e50ac380c015c30f4
ifrah qazi <ifrahqazi11@gmail.com>
2023-07-20 14:03:38 +0200

    added const file for src

---
+++ osm-geojson-api/src/const.ts
@@ -0,0 +1,11 @@
+export const OSM_API_URL = 'https://www.openstreetmap.org/api/0.6/map';
+
+export const API_ERRORS = {
+    UnableToRetreiveData : "UnableToRetreiveData",
+    BoundingParametersMissing: "BoundingParametersMissing"
+}
+
+export const API_ERROR_MESSAGES = {
+    BoundingParametersMissing : "Bounding box parameters missing. Please provide left, bottom, right, and top.",
+    UnableToRetreiveData : "Unable to retrieve OSM data."
+}
